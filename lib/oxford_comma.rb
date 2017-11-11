377a47056c604a314f58be505f5bc0d5aa1fd196
Leslie Hsu <chuckleberries@hotmail.com>
2017-11-11 20:39:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.length = 1
+  if array.length == 1
     array.to_s
-  elsif array.length = 2
+  elsif array.length == 2
     array.join(" and ")
   else
     last = array.pop
